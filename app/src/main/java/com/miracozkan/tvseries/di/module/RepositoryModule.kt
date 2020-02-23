e43f955074a2acf62c043eda7d10d694be42ae15
Mirac Ozkan <mirac.ozkan123@gmail.com>
2020-02-23 12:45:26 +0300

    add "module" annotation to repo module

--- app/src/main/java/com/miracozkan/tvseries/di/module/RepositoryModule.kt
+++ app/src/main/java/com/miracozkan/tvseries/di/module/RepositoryModule.kt
@@ -3,6 +3,7 @@ package com.miracozkan.tvseries.di.module
 import com.miracozkan.tvseries.repository.SeriesRepository
 import com.miracozkan.tvseries.repository.SeriesRepositoryImpl
 import dagger.Binds
+import dagger.Module
 
 
 // Code with ❤
@@ -14,6 +15,7 @@ import dagger.Binds
 //│ 23.02.2020 - 12:42          │
 //└─────────────────────────────┘
 
+@Module
 abstract class RepositoryModule {
     @Binds
     abstract fun provideSeriesRepository(seriesRepositoryImpl: SeriesRepositoryImpl): SeriesRepository
