9b713a68a19b68ed4452c82e178819b75cc5aa7c
Mirac Ozkan <mirac.ozkan123@gmail.com>
2020-02-23 12:34:31 +0300

    change variable type var with val

--- app/src/main/java/com/miracozkan/tvseries/datalayer/model/PopularSeriesResult.kt
+++ app/src/main/java/com/miracozkan/tvseries/datalayer/model/PopularSeriesResult.kt
@@ -21,45 +21,44 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class PopularSeriesResult(
     @PrimaryKey(autoGenerate = true)
-    var sid: Int? = null,
-
+    val sid: Int,
     @SerializedName("original_name")
     @Expose
-    var originalName: String? = null,
+    val originalName: String,
     @SerializedName("genre_ids")
     @Expose
-    var genreIds: List<Int>? = null,
+    val genreIds: List<Int>? = null,
     @SerializedName("name")
     @Expose
-    var name: String? = null,
+    val name: String? = null,
     @SerializedName("popularity")
     @Expose
-    var popularity: Float? = null,
+    val popularity: Float? = null,
     @SerializedName("origin_country")
     @Expose
-    var originCountry: List<String>? = null,
+    val originCountry: List<String>? = null,
     @SerializedName("vote_count")
     @Expose
-    var voteCount: Int? = null,
+    val voteCount: Int? = null,
     @SerializedName("first_air_date")
     @Expose
-    var firstAirDate: String? = null,
+    val firstAirDate: String? = null,
     @SerializedName("backdrop_path")
     @Expose
-    var backdropPath: String? = null,
+    val backdropPath: String? = null,
     @SerializedName("original_language")
     @Expose
-    var originalLanguage: String? = null,
+    val originalLanguage: String? = null,
     @SerializedName("id")
     @Expose
-    var id: Int? = null,
+    val id: Int? = null,
     @SerializedName("vote_average")
     @Expose
-    var voteAverage: Float? = null,
+    val voteAverage: Float? = null,
     @SerializedName("overview")
     @Expose
-    var overview: String? = null,
+    val overview: String? = null,
     @SerializedName("poster_path")
     @Expose
-    var posterPath: String? = null
+    val posterPath: String? = null
 ) : Parcelable
