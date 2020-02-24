92d8cdd3c9aeb1c98a3efc18820288cf69acc32b
Mirac Ozkan <mirac.ozkan123@gmail.com>
2020-02-24 19:59:23 +0300

    settings up nav component

--- app/src/main/java/com/miracozkan/tvseries/ui/activity/MainActivity.kt
+++ app/src/main/java/com/miracozkan/tvseries/ui/activity/MainActivity.kt
@@ -1,84 +1,23 @@
 package com.miracozkan.tvseries.ui.activity
 
 import android.os.Bundle
-import android.view.WindowManager
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.miracozkan.tvseries.R
-import com.miracozkan.tvseries.adapter.TrailerPagerAdapter
-import com.miracozkan.tvseries.base.BaseActivity
-import com.miracozkan.tvseries.datalayer.network.RetrofitClient
-import com.miracozkan.tvseries.utils.*
-import com.miracozkan.tvseries.viewmodel.PopularSeriesViewModel
-import kotlinx.android.synthetic.main.activity_main.*
+import com.miracozkan.tvseries.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private val popularSeriesRepository by lazy {
-        DependencyUtil.getPopularSeriesRepository(
-            RetrofitClient.getClient()
-        )
-    }
-
-    private val popularSeriesViewModel by lazy {
-        ViewModelProviders.of(
-            this,
-            ViewModelFactory(popularSeriesRepository)
-        ).get(PopularSeriesViewModel::class.java)
-    }
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        initUi()
-        initObserver()
-    }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-    private fun initUi() {
-
-        /**
-         * For Fullscreen Videos
-         */
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-
-        vpTrailers.apply {
-            adapter = TrailerPagerAdapter(supportFragmentManager)
-            offscreenPageLimit = 3
-        }
-    }
-
-    private fun initObserver() {
-        popularSeriesViewModel.popularSeriesList.observe(this, Observer { _resource ->
-            when (_resource.status) {
-                Status.LOADING -> {
-                    showProgress()
-                }
-                Status.ERROR -> {
-                    hideProgress()
-                    Toast.makeText(this, _resource.message, Toast.LENGTH_SHORT).show()
-                }
-                Status.SUCCESS -> {
-                    hideProgress()
-                    (vpTrailers.adapter as TrailerPagerAdapter).setVideoList(_resource.data.orEmpty())
-                }
-            }
-        })
-
-    }
-
-    /**
-     * This interface can be unnecessary
-     */
-    private fun hideProgress() {
-        prg.hideProgress()
-    }
-
-    private fun showProgress() {
-        prg.showProgress()
+        navController = findNavController(R.id.nav_host_fragment)
     }
 }
