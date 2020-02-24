c242c4596924adda053d3cdf395150e712530203
Mirac Ozkan <mirac.ozkan123@gmail.com>
2020-02-24 19:57:44 +0300

    inject dependency

---
+++ app/src/main/java/com/miracozkan/tvseries/MyApplication.kt
@@ -0,0 +1,37 @@
+package com.miracozkan.tvseries
+
+import android.app.Application
+import com.miracozkan.tvseries.di.component.DaggerAppComponent
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+
+// Code with ❤
+//┌─────────────────────────────┐
+//│ Created by Mirac OZKAN      │
+//│ ─────────────────────────── │
+//│ mirac.ozkan123@gmail.com    │
+//│ ─────────────────────────── │
+//│ 23.02.2020 - 15:59          │
+//└─────────────────────────────┘
+
+class MyApplication : Application(), HasAndroidInjector {
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return androidInjector
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        DaggerAppComponent
+            .builder()
+            .application(this)
+            .build()
+            .inject(this)
+    }
+}
